e07b6eb89fb06f64f68b071528a4243ab528f0ac
Joshua Farinmade <joshuafarinmade@gmail.com>
2026-03-23 16:08:54 +0100

    day 22 task

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -1,6 +1,12 @@
 from pathlib import Path
 import json
 
+import plotly.express as px
+import plotly.io as pio
+
+
+pio.renderers.default='browser'
+
 # Read data as a string and convert to a python object
 path = Path('eq_data/eq_data_1_day_m1.geojson')
 contents= path.read_text(encoding='utf-8', errors='ignore')
@@ -14,4 +20,21 @@ path.write_text(readable_contents)
 # print(contents[:1000])
 # Examine all earthquakes in the dataset
 all_eq_dict =all_eq_data['features']
-print(len(all_eq_dict))
+# print(len(all_eq_dict))
+mags, lons, lats =[], [], []
+for eq_dict in all_eq_dict:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+
+title ="Global Earthquake"
+fig= px.scatter_geo(lat=lats, lon=lons, title=title)
+fig.show()
+# fig.write_image("earthquake_map.html")
+    # print(mags[:10])
+    # print(lons[:5])
+    # print(lats[:5])
+    #
